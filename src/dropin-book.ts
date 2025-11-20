1d5cb541aadc94baf66f0543d51ffe62da977883
Jason Sun <jasonsunjishen@gmail.com>
2025-11-19 23:33:47 -0500

    Update dropin and profile config process

--- src/dropin-book.ts
+++ src/dropin-book.ts
@@ -1,27 +1,74 @@
 import { chromium } from "playwright";
 import "dotenv/config";
 import type { Page } from "playwright";
+import dropinConfig from "../dropin.config.json" with { type: "json" };
+import events from "../dropin-events.json" with { type: "json" };
 
 const CONFIG = {
     baseUrl: "https://townofoakville.perfectmind.com/",
-    username: process.env.JASON_USERNAME!,
-    password: process.env.JASON_PASSWORD!,
     loggedInUrl: /\/MyProfile\/Contact(?:\/|$)/,
     timeout: 8000, // MS
     headless: false,
     refreshInterval: 2000 // MS
 };
 
-const DROPIN = {
-    sport: "Basketball",
+const eventName = dropinConfig["event-name"];
+const eventConfig = events.find(e => e.name == eventName);
+const PROFILE = {
+    username: process.env[`${dropinConfig.name.toUpperCase()}_USERNAME`]!,
+    password: process.env[`${dropinConfig.name.toUpperCase()}_PASSWORD`]!,
+}
 
+if (!eventConfig) {
+    throw new Error("Cannot find drop-in event");
+}
+
+if (!PROFILE.username || !PROFILE.password) {
+    throw new Error("Invalid environment variables username or password");
+}
+
+try {
+    await main();
+} catch (e) {
+    console.error("Error: ", e);
+}
+
+async function main() {
+    console.log("Start");
+
+    const browser = await chromium.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(CONFIG.baseUrl, { waitUntil: "domcontentloaded" });
+
+    await login(page);
+    
+    const dropIn = page.getByRole('link', { name: "Drop-In Programs" });
+
+    if (await dropIn.isVisible()) {
+        await dropIn.click();
+    } else {
+        const moreMenu = page.getByRole('menuitem', { name: "More" });
+        await moreMenu.hover();
+        await dropIn.waitFor({ state: 'visible' });
+        await dropIn.click();
+    }
+
+    const selectionList = page.locator('ul[data-bind*="foreach: calendars"]');
+    await selectionList.getByText('Sports Drop-in').click();
+
+    await register(page);
+}
+
+async function register(page: Page) {
+    
 }
 
 async function login(page: Page) {
     console.log("Login");
 
-    await page.locator('#username').fill(CONFIG.username);
-    await page.locator('#password').fill(CONFIG.password);
+    await page.locator('#username').fill(PROFILE.username);
+    await page.locator('#password').fill(PROFILE.password);
 
     const error = page
         .locator('#error')
@@ -42,21 +89,4 @@ async function login(page: Page) {
     if (result == "success") return;
 
     throw new Error("Login attempt timed out");
-}
-
-async function main() {
-    console.log("Start");
-
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
-
-    await page.goto(CONFIG.baseUrl, { waitUntil: "domcontentloaded" });
-    await login(page);
-    
-}
-
-try {
-    await main();
-} catch (e) {
-    console.error("Error: ", e);
 }
