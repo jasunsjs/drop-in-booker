009ac31062855a8de1e0efe6145847612a4b4bb9
Jason Sun <jasonsunjishen@gmail.com>
2025-11-22 22:26:42 -0500

    Locate the target date header and event

--- src/dropin-book.ts
+++ src/dropin-book.ts
@@ -12,56 +12,62 @@ const CONFIG = {
     refreshInterval: 2000 // MS
 };
 
-const eventName = dropinConfig["event-name"];
-const eventConfig = events.find(e => e.name == eventName);
 const PROFILE = {
     username: process.env[`${dropinConfig.name.toUpperCase()}_USERNAME`]!,
     password: process.env[`${dropinConfig.name.toUpperCase()}_PASSWORD`]!,
 }
 
-if (!eventConfig) {
-    throw new Error("Cannot find drop-in event");
-}
+const DOW_INDEX: Record<string, number> = {
+    Sunday: 0,
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6,
+} as const;
 
-if (!PROFILE.username || !PROFILE.password) {
-    throw new Error("Invalid environment variables username or password");
-}
+type EventConfig = {
+    name: string;
+    sport: string;
+    location: string;
+    day: string; // Day of the week
+    time: string; // "HH:MM" 24h
+};
 
-try {
-    await main();
-} catch (e) {
-    console.error("Error: ", e);
-}
+type DropinConfig = {
+    name: string;
+    "event-name": string;
+    "book-for-tmr": boolean;
+};
 
-async function main() {
-    console.log("Start");
+function to12Hour(time24: string): string {
+    const [hStr, m] = time24.split(':');
+    if (hStr && m) {
+        const hour = parseInt(hStr, 10);
+        const suffix = hour >= 12 ? "pm" : "am";
+        const hour12 = hour % 12 === 0 ? 12 : hour % 12;
 
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
-
-    await page.goto(CONFIG.baseUrl, { waitUntil: "domcontentloaded" });
-
-    await login(page);
-    
-    const dropIn = page.getByRole('link', { name: "Drop-In Programs" });
-
-    if (await dropIn.isVisible()) {
-        await dropIn.click();
+        return `${hour12}:${m} ${suffix}`;
     } else {
-        const moreMenu = page.getByRole('menuitem', { name: "More" });
-        await moreMenu.hover();
-        await dropIn.waitFor({ state: 'visible' });
-        await dropIn.click();
+        throw new Error(`Invalid time format: ${time24}`);
     }
-
-    const selectionList = page.locator('ul[data-bind*="foreach: calendars"]');
-    await selectionList.getByText('Sports Drop-in').click();
-
-    await register(page);
 }
 
-async function register(page: Page) {
-    
+function getTargetDate(eventConfig: EventConfig): Date {
+    const today = new Date();
+    const target = new Date(today);
+    const todayDOW = today.getDay();
+    const targetDOW = DOW_INDEX[eventConfig.day]!;
+    let diff = targetDOW - todayDOW;
+    if (diff < 0) 
+        diff += 7; // Calculate number of days ahead 
+
+    if (diff == 1 && !dropinConfig["book-for-tmr"]) 
+        diff += 7;
+
+    target.setDate(target.getDate() + diff);
+    return target;
 }
 
 async function login(page: Page) {
@@ -90,3 +96,67 @@ async function login(page: Page) {
 
     throw new Error("Login attempt timed out");
 }
+
+async function register(page: Page, eventConfig: EventConfig) {
+    console.log("Register");
+    if (!eventConfig) {
+        throw new Error("Invalid drop-in event");
+    }
+    console.log(eventConfig);
+    
+    page.locator('#load-more').click();
+
+    const targetDate = getTargetDate(eventConfig);
+    const weekdayHeader = targetDate.toLocaleDateString("en-US", { weekday: "short"});
+    const monthHeader = targetDate.toLocaleDateString("en-US", { month: "short"});
+    const targetDateHeader = `${weekdayHeader}, ${monthHeader} ${targetDate.getDate()}`;
+    console.log(`Target Date Header: ${targetDateHeader}`);
+
+
+
+
+    const rows = page.locator('#classes tr.bm-class-row');
+
+}
+
+async function main() {
+    console.log("Start");
+
+    const eventName = dropinConfig["event-name"];
+    const eventConfig = events.find(e => e.name == eventName);
+    if (!eventConfig) throw new Error("Invalid drop-in event");
+    
+    const browser = await chromium.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(CONFIG.baseUrl, { waitUntil: "domcontentloaded" });
+
+    await login(page);
+    
+    const dropIn = page.getByRole('link', { name: "Drop-In Programs" });
+
+    if (await dropIn.isVisible()) {
+        await dropIn.click();
+    } else {
+        const moreMenu = page.getByRole('menuitem', { name: "More" });
+        await moreMenu.hover();
+        await dropIn.waitFor({ state: 'visible' });
+        await dropIn.click();
+    }
+
+    const selectionList = page.locator('ul[data-bind*="foreach: calendars"]');
+    await selectionList.getByText('Sports Drop-in').click();
+
+    await register(page, eventConfig);
+}
+
+if (!PROFILE.username || !PROFILE.password) {
+    throw new Error("Invalid environment variables username or password");
+}
+
+try {
+    await main();
+} catch (e) {
+    console.error("Error: ", e);
+}
+
